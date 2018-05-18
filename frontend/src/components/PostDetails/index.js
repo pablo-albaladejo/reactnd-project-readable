04fa092f1f0b57087525fd4948f757abf0d685a0
Pablo Albaladejo Mestre <pablo.albaladejo.mestre@gmail.com>
2018-05-18 19:37:39 +0200

    Feature: Post update voteScore at detailed post screen

--- frontend/src/components/PostDetails/index.js
+++ frontend/src/components/PostDetails/index.js
@@ -1,13 +1,28 @@
 
 import React, { Component } from 'react';
 
+import { connect } from 'react-redux';
+
+import VoteScore from '../VoteScore'
+import {
+    updatePostVoteScore,
+} from '../../actions/';
+
 import {
     Card, CardText, CardBody,
-    CardTitle, CardSubtitle, 
+    CardTitle, CardSubtitle,
     //Button
 } from 'reactstrap';
 
 class PostDetails extends Component {
+
+    onDownVote = (postId) => {
+        this.props.dispatch(updatePostVoteScore(postId, false));
+    }
+
+    onUpVote = (postId) => {
+        this.props.dispatch(updatePostVoteScore(postId, true));
+    }
 
     render() {
         let post = this.props.item;
@@ -16,7 +31,15 @@ class PostDetails extends Component {
                 {post && (
                     <Card>
                         <CardBody>
-                            <CardTitle>{post.title}</CardTitle>
+                            <CardTitle>
+                                <VoteScore
+                                    id={post.id}
+                                    value={post.voteScore}
+                                    onDownVote={this.onDownVote}
+                                    onUpVote={this.onUpVote}
+                                />
+                                {post.title}
+                            </CardTitle>
                             <CardSubtitle>{post.author}</CardSubtitle>
                             <CardText>{post.body}</CardText>
                             {/* <Button>Button</Button> */}
@@ -27,4 +50,4 @@ class PostDetails extends Component {
         );
     }
 }
-export default PostDetails;
+export default connect()(PostDetails);
