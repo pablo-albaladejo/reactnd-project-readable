4c4d645c3785d67983b8885b310af392443ed2a1
Pablo Albaladejo Mestre <pablo.albaladejo.mestre@gmail.com>
2018-05-26 15:29:47 +0200

    refactor: delete unused style files

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 
 import { BrowserRouter } from 'react-router-dom';
 
-import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'font-awesome/css/font-awesome.min.css';
 
