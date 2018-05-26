4cfae4f91061c7ecb641ee19a1625e3e00370c33
Pablo Albaladejo Mestre <pablo.albaladejo.mestre@gmail.com>
2018-05-26 15:32:23 +0200

    feat: VoteScore component styled

--- frontend/src/components/VoteScore/index.js
+++ frontend/src/components/VoteScore/index.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 
-class VoteScore extends Component {    
+import { css } from 'aphrodite';
+import styles from './styles';
 
-    render(){
-        return(
+class VoteScore extends Component {
+
+    render() {
+        return (
             <div>
-                <i className="fa fa-arrow-up" onClick={() => this.props.onUpVote(this.props.id)}/>
-                {this.props.value}
-                <i className="fa fa-arrow-down" onClick={() => this.props.onDownVote(this.props.id)}/>
+                <i className={"fa fa-arrow-up" + " " + css(styles.arrow)} onClick={() => this.props.onUpVote(this.props.id)} />
+                <span className={css(styles.counter)} >{this.props.value}</span>
+                <i className={"fa fa-arrow-down" + " " + css(styles.arrow)} onClick={() => this.props.onDownVote(this.props.id)} />
             </div>
         );
     }
---
+++ frontend/src/components/VoteScore/styles.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'aphrodite';
+import Colors from '../../config/Colors';
+
+const styles = StyleSheet.create({
+    arrow: {
+        color: Colors.font,
+    },
+    counter: {
+        color: Colors.primary,
+    }
+})
+export default styles;
