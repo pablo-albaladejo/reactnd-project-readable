80c5278127c16e0c2414979c58a5235f41fef363
Pablo Albaladejo Mestre <pablo.albaladejo.mestre@gmail.com>
2018-05-26 15:31:01 +0200

    feat: global colors file added

---
+++ frontend/src/config/Colors.js
@@ -0,0 +1,14 @@
+const Colors = {
+    primary: "#FF9E1B",
+    secondary: "#41B6E6",
+    success: "#4dbd74",
+    info: "#63c2de",
+    warning: "#ffc107",
+    danger: "#f86c6b",
+    light: "#efefef",
+    dark: "#29363d",
+    
+    clear: "#fff",
+    font: "#c7c9c7",
+}
+export default Colors;
