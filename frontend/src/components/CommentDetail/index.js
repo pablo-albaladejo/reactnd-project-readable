dbee8723f3186c5ef6cfc0641d99c6fe76422801
Pablo Albaladejo Mestre <pablo.albaladejo.mestre@gmail.com>
2018-05-27 14:11:47 +0200

    refactor: Comment Details refactored into a component

---
+++ frontend/src/components/CommentDetail/index.js
@@ -0,0 +1,97 @@
+
+
+import React, { Component } from 'react';
+
+import {
+    Card, CardText, CardBody,
+    CardSubtitle,
+    FormGroup, Input
+} from 'reactstrap';
+
+import { connect } from 'react-redux';
+
+import VoteScore from '../VoteScore';
+import {
+    updateCommentVoteScore,
+    deleteComment
+} from '../../actions/';
+
+import EditButton from '../Buttons/Edit';
+import DeleteButton from '../Buttons/Delete';
+
+
+import { css } from 'aphrodite';
+import styles from './styles';
+
+class CommentDetail extends Component {
+
+    state = {
+        author: null,
+        body: null,
+        id: null,
+        voteScore: null,
+    }
+
+    componentWillMount() {
+        this.setState({
+            author: this.props.comment.author,
+            body: this.props.comment.body,
+            id: this.props.comment.id,
+            voteScore: this.props.comment.voteScore,
+        });
+    }
+
+    onDownVote = (commentId) => {
+        this.props.dispatch(updateCommentVoteScore(commentId, false));
+    }
+
+    onUpVote = (commentId) => {
+        this.props.dispatch(updateCommentVoteScore(commentId, true));
+    }
+
+    oEditComment = (commentId) => {
+
+    }
+
+    onDeleteComment = (commentId) => {
+        this.props.dispatch(deleteComment(commentId));
+    }
+
+    render() {
+
+        return (
+            <Card>
+                <CardBody>
+                    <CardSubtitle><span className={css(styles.text)}>{this.state.author}</span></CardSubtitle>
+                    
+                    <FormGroup>
+                        <Input
+                            readOnly={!this.props.editing}
+                            type="textarea"
+                            name="postBody"
+                            id="postBody"
+                            value={this.state.body}
+                            onChange={(event) => {
+                                this.setState({
+                                    body: event.target.value,
+                                })
+                            }}
+                        />
+                    </FormGroup>
+
+                    <VoteScore
+                        id={this.state.id}
+                        value={this.state.voteScore}
+                        onDownVote={this.onDownVote}
+                        onUpVote={this.onUpVote}
+                    />
+                    <div>
+                        <EditButton onClick={() => this.oEditComment(this.state.id)} />{' '}
+                        <DeleteButton onClick={() => this.onDeleteComment(this.state.id)} />
+                    </div>
+                </CardBody>
+            </Card>
+        );
+    }
+}
+export default connect()(CommentDetail);
--- frontend/src/components/CommentList/index.js
+++ frontend/src/components/CommentList/index.js
@@ -1,68 +1,21 @@
 
 import React, { Component } from 'react';
 
-import {
-    Card, CardText, CardBody,
-    CardSubtitle,
-} from 'reactstrap';
-
-import { connect } from 'react-redux';
-
-import VoteScore from '../VoteScore';
-import {
-    updateCommentVoteScore,
-    deleteComment
-} from '../../actions/';
-
-import EditButton from '../Buttons/Edit';
-import DeleteButton from '../Buttons/Delete';
+import CommentDetail from '../CommentDetail';
 
 class CommentList extends Component {
-
-    onDownVote = (commentId) => {
-        this.props.dispatch(updateCommentVoteScore(commentId, false));
-    }
-
-    onUpVote = (commentId) => {
-        this.props.dispatch(updateCommentVoteScore(commentId, true));
-    }
-
-    oEditComment = (commentId) => {
-
-    }
-
-    onDeleteComment = (commentId) => {
-        this.props.dispatch(deleteComment(commentId));
-    }
 
     render() {
         let comments = this.props.item;
         return (
             <div>
-                {comments.map((comment, index) => {
+                {comments.map((item, index) => {
                     return (
-                        <Card key={index}>
-                            <CardBody>
-                                <CardSubtitle>{comment.author}</CardSubtitle>
-                                <CardText>
-                                    {comment.body}
-                                </CardText>
-                                <VoteScore
-                                    id={comment.id}
-                                    value={comment.voteScore}
-                                    onDownVote={this.onDownVote}
-                                    onUpVote={this.onUpVote}
-                                />
-                            </CardBody>
-                            <div>
-                                <EditButton onClick={() => this.oEditComment(comment.id)} />{' '}
-                                <DeleteButton onClick={() => this.onDeleteComment(comment.id)} />
-                            </div>
-                        </Card>
+                        <CommentDetail  key={index} comment={item}/>
                     );
                 })}
             </div>
         );
     }
 }
-export default connect()(CommentList);
+export default CommentList;
